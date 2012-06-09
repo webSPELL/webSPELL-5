9d0d6de28b80e9b11de14d506d237ee7d145c9a6
Philipp Rehs <P.Rehs@gmx.net>
2012-06-09 13:16:20 +0200

    added factory to get the right object of captcha, http, mail

---
+++ core/classes/Factory.php
@@ -0,0 +1,39 @@
+<?php
+class Factory {
+	static public function getMail(){
+		$registry = Regstry::single();
+		$mysqli = $registry->get('db');
+		$type = $this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='mail.type'");
+		if(class_exists($this->type)){
+			return new $type();
+		}
+		else {
+			throw new Exception("Unknown mail config");
+		}
+	}
+	
+	static public function getHttpRequest(){
+		$registry = Regstry::single();
+		$mysqli = $registry->get('db');
+		$type = $this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='httpRequest.type'");
+		if(class_exists($this->type)){
+			return new $type();
+		}
+		else {
+			throw new Exception("Unknown httprequest config");
+		}
+	}
+	
+	static public function getCaptcha(){
+		$registry = Regstry::single();
+		$mysqli = $registry->get('db');
+		$type = $this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='captcha.type'");
+		if(class_exists($this->type)){
+			return new $type();
+		}
+		else {
+			throw new Exception("Unknown captcha config");
+		}
+	}
+	
+}
