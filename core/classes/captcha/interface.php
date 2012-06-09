467e74f055aaf0a40548cc0bc3a7f31e4899b607
Philipp Rehs <P.Rehs@gmx.net>
2012-06-09 13:15:02 +0200

    moved captcha classes in seperate files

---
+++ core/classes/captcha/interface.php
@@ -0,0 +1,10 @@
+<?php
+abstract class Captcha_Interface {
+	private $mysqli;
+	private $registry;
+	public function __construct(){
+	}
+	abstract function create();
+	abstract function validate();
+}
+?>
