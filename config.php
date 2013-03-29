264babb32177a79e492a28ae9147dc103cc9aada
Florian Siegmund <mail@florian-siegmund.de>
2013-03-29 16:09:19 +0100

    db user changed

--- config.php
+++ config.php
@@ -4,14 +4,14 @@
 $database = Array(
     'type'      => 'mysql',
     'host'      => 'localhost',
-    'user'  => 'webspell',
+    'user'      => 'webspell5',
     'password'  => 'a08s09d10d11b12m13',
     'dbname'    => 'webspell5'
 );
 
 // minimal global configuration
 
-define('DEBUG', );  // 0 = no debug, 1 = errors, 2 = errors & warnings
+define('DEBUG', 2);  // 0 = no debug, 1 = errors, 2 = errors & warnings
 define('ADMIN', 'bluetiger@webspell.org');  // mail address of admin for error notifications
 
 ?>
