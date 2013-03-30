18b92728e170565bddab8b6968bd53bed0852968
Florian Siegmund <mail@florian-siegmund.de>
2013-03-30 23:09:17 +0100

    coding style fixes

--- src/core/classes/captcha/math.php
+++ src/core/classes/captcha/math.php
@@ -1,21 +1,23 @@
 <?php
 class Captcha_Math extends Captcha_Interface {
-    public function __construct(){
+
+    public function __construct() {
         parent::__construct();
     }
-    protected function createString(){
-        $first = rand(1,pow(10,($this->length-2) / 2)-1);
+
+    protected function createString() {
+        $first = rand(1, pow(10, ($this->length-2) / 2)-1);
         $captchastring = $first;
         $captchastring_show = (string) $first;
-        if(rand(0,1)){
+        if(rand(0, 1)) {
             $captchastring_show .= "+";
-            $next = rand(1,pow(10,($this->length-2) / 2)-1);
+            $next = rand(1, pow(10, ($this->length-2) / 2)-1);
             $captchastring += $next;
             $captchastring_show .= $next;
         }
-        else{
+        else {
             $captchastring_show .= "-";
-            $next = rand(1,$first);
+            $next = rand(1, $first);
             $captchastring -= $next;
             $captchastring_show .= $next;
         }
--- src/core/classes/captcha/recaptcha.php
+++ src/core/classes/captcha/recaptcha.php
@@ -1,5 +1,5 @@
 <?php
-class Captcha_ReCaptcha extends Captcha_Interface{
+class Captcha_ReCaptcha extends Captcha_Interface {
     /**
      * Api Key from Recaptcha
      * http://recaptcha.net/api/getkey
@@ -23,32 +23,32 @@ class Captcha_ReCaptcha extends Captcha_Interface{
      * @var array
      */
     private $servers = array(
-            'verify'=>'api-verify.recaptcha.net',
-            'script'=>'http://api.recaptcha.net',
-            'script_ssl'=>'https://api-secure.recaptcha.net');
+            'verify' => 'api-verify.recaptcha.net',
+            'script' => 'http://api.recaptcha.net',
+            'script_ssl' => 'https://api-secure.recaptcha.net');
     /**
      * The Theme which is used
      * @var string
      */
     private $theme = 'clean';
 
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
         $allow_url_fopen=@ini_get('allow_url_fopen');
         if($allow_url_fopen != 'On' && $allow_url_fopen =! 1) {
-            throw new Exception("URL FOPEN WRAPPER: allow_url_fopen is deactivated in your php configuration");
+            throw new WebspellException("URL FOPEN WRAPPER: allow_url_fopen is deactivated in your php configuration");
         }
         $this->publicKey = '6LcOIQwAAAAAALGHVrUGCEc6lu2QDdwnw8SFL6jp';//$this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='captcha_recaptcha_publicKey'");
         $this->privateKey = '6LcOIQwAAAAAAKAasQ8vrtIhhETEI2iLkrqOm-tX'; //$this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='captcha_recaptcha_privateKey'");
         $this->useSSL = false;//(boolean)$this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='captcha_recaptcha_useSSL'");
         //$this->theme = $this->mysqli->querySingleValue("SELECT `value` FROM ".DB_PREFIX."system_vars WHERE `field`='captcha_recaptcha_theme'");
-        if($this->publicKey == '' || $this->privateKey == ''){
-            throw new Exception("RECAPTCHA ERROR: You need a Api Key to use. http://recaptcha.net/api/getkey");
+        if($this->publicKey == '' || $this->privateKey == '') {
+            throw new WebspellException("RECAPTCHA ERROR: You need a Api Key to use. http://recaptcha.net/api/getkey");
         }
     }
 
-    public function create(){
-        if($this->useSSL){
+    public function create() {
+        if($this->useSSL) {
             $server = $this->servers['script_ssl'];
         }
         else{
@@ -67,27 +67,27 @@ class Captcha_ReCaptcha extends Captcha_Interface{
     <input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
     </noscript>';
     }
-    public function validate(){
-        if(empty($_POST['recaptcha_challenge_field']) ||
-                empty($_POST['recaptcha_response_field'])){
+
+    public function validate() {
+        if(empty($_POST['recaptcha_challenge_field']) || empty($_POST['recaptcha_response_field'])) {
             return false;
         }
         $http = new HttpRequest();
-        if($http){
+        if($http) {
             $http->setHost($this->servers['verify']);
             $http->setPath("/verify");
             $http->setData(array ('privatekey' => $this->privateKey,
                     'remoteip' => $_SERVER['REMOTE_ADDR'],
                     'challenge' => $_POST['recaptcha_challenge_field'],
                     'response' => $_POST['recaptcha_response_field']
-            )
+                )
             );
             $http->setUserAgent("reCAPTCHA/PHP");
             $response = $http->post();
-            $data = explode("\n",$response);
+            $data = explode("\n", $response);
             return ($data[0] == "true");
         }
-        else{
+        else {
             return false;
         }
     }
