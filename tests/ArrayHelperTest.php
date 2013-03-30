a55b30b3d37cf0c8dc871fbf081f3323330bfaa4
Philipp Rehs <P.Rehs@gmx.net>
2013-03-30 14:25:38 +0100

    Test added for ArrayHelper

---
+++ tests/ArrayHelperTest.php
@@ -0,0 +1,22 @@
+<?php
+class ArrayHelperTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testAssocArray()
+    {
+      $array = array();
+      $array["test"]="Test";
+      $array["key"]="Value";
+      $this->assertEquals(true, ArrayHelper::is_assoc($array));
+    }
+
+    public function testIndexedArray()
+    {
+      $array = array();
+      $array[0]="Test";
+      $array[1]="Value";
+      $array[2]="Value";
+      $this->assertEquals(false, ArrayHelper::is_assoc($array));
+    }
+}
+?>
